08f601da7517f6f2a398338c422b7f29cf3f7993
marina farias <marinaalfarias@gmail.com>
2025-12-21 00:06:25 -0300

    Primeira versão do site see

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+console.log("Site da SEEE Unicamp carregado com sucesso!");
